cd18986216fb6237de63f700cf76b2478a97749f
Krzysztof Zabski <froschkris@gmail.com>
2022-04-24 00:05:02 +0200

    feat: trade sundays finding function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function sunday(){
+function sunday() {
 
-    let log  = (varb) => console.log(varb);
+    let lg = (varb) => console.log(varb);
 
     let today = new Date();
     let tradeSundays = new Array();
@@ -10,9 +10,39 @@ function sunday(){
     tradeSundays[2] = new Date(2022, 07, 28);
     tradeSundays[3] = new Date(2022, 11, 12);
     tradeSundays[4] = new Date(2022, 11, 18);
+    
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
 
-    if(today.getDay() == 6){
-        log('simea');
+    if (today.getDay() == 6) {
+
+        let isMonth = new Array();
+
+        tradeSundays.forEach((element, index) => {
+
+            isMonth[index] = tomorrow.getMonth() === element.getMonth();
+
+            if (isMonth[index] == true) {
+
+                let isDate = tomorrow.getDate() === element.getDate();
+
+                if (isDate == true) {
+                    alert('jutro handlowa!!!');
+                } else {
+                    alert('no raczej nie');
+                }
+            }
+        });
+    } else {
+
+        switch (tomorrow.getDay()){
+            
+            case 1:
+                
+
+
+        }
+
     }
 
 }
